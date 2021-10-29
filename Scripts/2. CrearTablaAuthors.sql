f379fbcd869a4a58a453f357f47ae934c47641b5
Harold Bartolo <habartolom@gmail.com>
2021-10-29 09:04:53 -0500

    scripts

---
+++ Scripts/2. CrearTablaAuthors.sql
@@ -0,0 +1,21 @@
+USE [test]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Authors](
+	[AuthorId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[BirthDate] [datetime2](7) NOT NULL,
+	[City] [nvarchar](max) NULL,
+	[Email] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Authors] PRIMARY KEY CLUSTERED 
+(
+	[AuthorId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Scripts/3. CrearTablaBooks.sql
@@ -0,0 +1,30 @@
+USE [test]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Books](
+	[BookId] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](max) NULL,
+	[Year] [int] NOT NULL,
+	[Genre] [nvarchar](max) NULL,
+	[Pages] [int] NOT NULL,
+	[AuthorId] [int] NOT NULL,
+ CONSTRAINT [PK_Books] PRIMARY KEY CLUSTERED 
+(
+	[BookId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Books]  WITH CHECK ADD  CONSTRAINT [FK_Books_Authors_AuthorId] FOREIGN KEY([AuthorId])
+REFERENCES [dbo].[Authors] ([AuthorId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[Books] CHECK CONSTRAINT [FK_Books_Authors_AuthorId]
+GO
